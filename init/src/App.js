2d33158ecd0d6df632e8e2296dde225325f5e529
hptg1994 <hptg1994@gmail.com>
2018-09-12 13:21:14 -0400

    add a title

--- init/src/App.js
+++ init/src/App.js
@@ -1,45 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import Section from './components/Section';
+import Wave from './components/Wave';
+import Header from './components/header'
+import Cell from './components/Cell';
+import styled from 'styled-components'
 
 class App extends Component {
   render() {
     return (
       <div>
-      <div className="Hero">
-        <div className="HeroGroup">
-          <h1>Learn to <br />design and code React apps</h1>
-          <p>Complete courses bout the best tools and design systems. Prototype and build apps with React and Swift</p>
-          <Link to="/page-2/">Watch the video</Link>
+      <Header />
+      <div className="Header">
+        <div className="HeaderGroup">
+          <h1>Pintaigao He<br/><span>The webdeveloper</span></h1>
+          <div className = "Header-subtitle">
+            <p>#Frontend</p>          
+            <p>#React</p>          
+            <p>#Flutter</p>          
+            <p>#Spring</p>          
+            <p>#Vue</p>          
+          </div>
           <div className="Logos">
-            <img src={require("./images/logo-sketch.png")} width="50" />
-            <img src={require("./images/logo-figma.png")} width="50" />
-            <img src={require("./images/logo-studio.png")} width="50" />
-            <img src={require("./images/logo-framer.png")} width="50" />
-            <img src={require("./images/logo-react.png")} width="50" />
-            <img src={require("./images/logo-swift.png")} width="50" />
+            <div>
+            <img src={require("./images/logo-js.png")}  height="50" />
+            <img src={require("./images/logo-python.png")}  height="50" />
+            <img src={require("./images/logo-java.png")}  height="50" />
+            <img src={require("./images/logo-sketch.png")}  height="50" />
+            <img src={require("./images/logo-zeplin.png")}  height="50" />
+            <img src={require("./images/logo-django.png")}  height="50" />
+            <img src={require("./images/logo-vscode.png")}  height="50" />
+            <img src={require("./images/logo-idea.png")}  height="50" />
+            <img src={require("./images/logo-webstorm.png")}  height="50" />
+            <img src={require("./images/logo-android-studio.png")}  height="50" />
+            <img src={require("./images/logo-react.png")}  height="50" />
+            <img src={require("./images/logo-vue.png")}  height="50" />
+            <img src={require("./images/logo-angular.png")}  height="50" />
+            <img src={require("./images/logo-flutter.png")}  height="50" />
+            <img src={require("./images/logo-dart.png")}  height="50" />
+            <img src={require("./images/logo-firebase.png")}  height="50" />
+            <img src={require("./images/logo-swift.png")}  height="50" />
+            <img src={require("./images/logo-xcode.png")}  height="50" />
+            <img src={require("./images/logo-rails.png")}  height="50" />
+            <img src={require("./images/logo-spring.png")}  height="50" />
+            <img src={require("./images/logo-postgres.png")}  height="50" />
+            <img src={require("./images/logo-teamsql.png")}  height="50" />
+            <img src={require("./images/logo-mysql.png")}  height="50" />
+            </div>
           </div>
           <Wave />
-  
-  
         </div>
       </div>
       <div className="Cards">
         <h2> 11 course, more coming</h2>
         <div className="CardGroup">
-          <Card title="Design System" text="10 section" image={require("../images/wallpaper.jpg")} />
-          <Card title="React for Designer" text="12 section" image={require("../images/wallpaper2.jpg")} />
-          <Card title="Sound Design" text="5 section" image={require("../images/wallpaper3.jpg")} />
-          <Card title="ARKit 2" text="10 section" image={require("../images/wallpaper4.jpg")} />
+          <Card title="Design System" text="10 section" image={require("./images/wallpaper.jpg")} />
+          <Card title="React for Designer" text="12 section" image={require("./images/wallpaper2.jpg")} />
+          <Card title="Sound Design" text="5 section" image={require("./images/wallpaper3.jpg")} />
+          <Card title="ARKit 2" text="10 section" image={require("./images/wallpaper4.jpg")} />
         </div>
       </div>
-      <Section image={require("../images/wallpaper2.jpg")} logo={require("../images/logo-react.png")} title="React for Designer" text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify " />
-      <SectionCaption>12 section - 6 hours</SectionCaption>
-      <SectionCellGroup>
+      {/* <Section image={require("../images/wallpaper2.jpg")} logo={require("../images/logo-react.png")} title="React for Designer" text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify " />
+      <SectionCaption>12 section - 6 hours</SectionCaption> */}
+      {/* <SectionCellGroup>
         {staticdata.cells.map(cell => (
           <Cell title={cell.title} image={cell.image} />
         ))}
-      </SectionCellGroup>
+      </SectionCellGroup> */}
     </div >
     );
   }
--- init/src/components/header.js
+++ init/src/components/header.js
@@ -25,13 +25,12 @@ class Header extends React.Component {
   }
   render() {
     return (
-      <div className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}>
-        <div className="HeaderGroup">
-          <Link to="/"><img src={require('../images/logo-designcode.svg')} alt="logo" width="30" /></Link>
-          <Link to="/course">Courses</Link>
-          <Link to="/downloads">Downloads</Link>
-          <Link to="/workshops">workshops</Link>
-          <Link to="/buy"><button>Buy</button></Link>
+      <div className={this.state.hasScrolled ? "Navigator NavigatorScrolled" : "Navigator"}>
+        <div className="NavigatorGroup">
+          <p>Courses</p>
+          <p>Downloads</p>
+          <p>workshops</p>
+          <p><button>Buy</button></p>
         </div>
       </div>
     )
