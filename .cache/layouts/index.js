4841af34d945e64dd4604bf635817a2de1aaceb0
hptg1994 <hptg1994@gmail.com>
2018-09-12 00:21:32 -0400

    initiate the packages

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/hptg/Documents/Projects/Web/Blog/hptg1994.github.io/src/layouts/index.js"
+  import data from "/Users/hptg/Documents/Projects/Web/Blog/hptg1994.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
