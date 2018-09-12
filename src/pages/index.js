a0fff6254a91394715c59afef5b81d7d16989f65
hptg1994 <hptg1994@gmail.com>
2018-09-12 14:39:23 -0400

    move to new folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,24 +30,6 @@ const SectionCellGroup = styled.div
 
 const IndexPage = () => (
   <div>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1>Learn to <br />design and code React apps</h1>
-        <p>Complete courses bout the best tools and design systems. Prototype and build apps with React and Swift</p>
-        <Link to="/page-2/">Watch the video</Link>
-        <div className="Logos">
-          <img src={require("../images/logo-sketch.png")} width="50" />
-          <img src={require("../images/logo-figma.png")} width="50" />
-          <img src={require("../images/logo-studio.png")} width="50" />
-          <img src={require("../images/logo-framer.png")} width="50" />
-          <img src={require("../images/logo-react.png")} width="50" />
-          <img src={require("../images/logo-swift.png")} width="50" />
-        </div>
-        <Wave />
-
-
-      </div>
-    </div>
     <div className="Cards">
       <h2> 11 course, more coming</h2>
       <div className="CardGroup">
