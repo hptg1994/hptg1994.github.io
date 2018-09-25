97b514cb1fd52727364ad2319359901fe94f34e3
hptg1994 <hptg1994@gmail.com>
2018-09-24 21:12:16 -0500

    adding a constraint

--- src/components/Projects/Projects-Todo.js
+++ src/components/Projects/Projects-Todo.js
@@ -6,9 +6,9 @@ class Projects_TODO extends Component {
     return (
       <div className = "projects-section">
         <h2 className="projects-title">TODO Mobile Client</h2>
-        <div className = "projects-img-container">
-          <img src= {require("../../images/screen-shot-gif.gif")} alt="viafly-dashboard" className = "projects-img"/>
-          <div className="project-detail" style = {{right:'1%'}}>
+        <div className = "projects-img-container" style = {{ width:"100%"}}>
+          <img src= {require("../../images/screen-shot-gif.gif")} alt="viafly-dashboard"/>
+          <div className="project-detail">
             <p>TODO Mobile Client (Implment Design)</p>
             <article>
               A Todo app help to track the waititng to do list.( I implement the Design !)
